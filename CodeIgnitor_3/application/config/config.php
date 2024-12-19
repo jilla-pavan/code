2a21ffa573a7d8316e155e17b887bb757cba1437
jilla-pavan <jillapavan22001@gmail.com>
2024-12-19 11:57:46 +0530

    git config changes done

--- CodeIgnitor_3/application/config/config.php
+++ CodeIgnitor_3/application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://localhost/Codeignitor_3/';
+$config['base_url'] = 'http://localhost/PHP-CodeIgniter-CRUD-Application/CodeIgnitor_3/';
 
 /*
 |--------------------------------------------------------------------------
